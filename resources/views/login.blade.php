52b27f165700de214656cc88f9ee068cbbcc98da
yoniffafan <yoniffafan@gmail.com>
2017-05-05 15:43:05 +0700

    1st

---
+++ resources/views/login.blade.php
@@ -0,0 +1,133 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="css/loginstyle.css" />
+<link rel="icon" type="image/jpg" href="http://img4.bp3padang.sch.id/unnamed-ac403-2530_67.png"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>HOME</title>
+  </head>
+  <body>
+
+    <script src="https://use.fontawesome.com/026ffd076e.js"></script>
+<div class="background-image"></div>
+
+<a class="navbar-brand boxbrand hidden-sm hidden-xs" href="#">
+<img src="http://img4.bp3padang.sch.id/unnamed-ac403-2530_67.png" alt="logo"/>
+<span>BP3 PADANG</span>
+</a>
+
+    <nav class="navbar navbar-findcond">
+    <div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+
+		</div>
+
+<div class="collapse navbar-collapse" id="navbar">
+  <ul class="nav navbar-nav navbar-right">
+    	<li class="active"><a href="/sia/login">SIA<span class="sr-only">(current)</span></a></li>
+      <li ><a href="#">E-Learning</a></li>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">About</a></li>
+  </ul>
+</div>
+</div>
+</nav>
+
+
+<div class="collapse navbar-collapse secondnavbar" id="navbarr">
+
+  <ul class="nav navbar-nav navbar-right navbardua">
+    <li><a href="/tentang">TENTANG KAMI</a></li>
+    <li><a href="#">PENDIDIKAN</a></li>
+    <li><a href="#">KEMAHASISWAAN</a></li>
+    <li><a href="#">PENELITIAN</a></li>
+    <li><a href="#">PENGABDIAN</a></li>
+    <li class="#"><a href="#">PENDAFTARAN</a></li>
+  </ul>
+
+</div>
+
+<div class="container containerstyle">
+  <section id="main-heading" class="section-page containerstyle">
+    <div class="container-fluid containerstyle">
+      <ul class="breadcrumbs breadcrumb">
+        <li>
+          <span><i class="fa fa-home"></i></span>
+          <a href="/">Beranda</a>
+          <span class="divider"></span>
+        </li>
+        {{-- <li class="active">
+          Tentang Kami
+        </li> --}}
+      </ul>
+      <div class="row">
+        <div class="col-md-8">
+          <div class="panel panel-default">
+        <div class="panel-heading"> <i class="fa fa-info-circle fa-fw" ></i> Informasi</div>
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      </div>
+        </div>
+        <div class="col-md-4">
+          <div class="panel panel-default">
+        <div class="panel-heading"><i class="fa fa-flag fa-fw" ></i> Login</div>
+        <div class="panel-body">
+          <form>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-user-circle-o fa-fw" ></i></span>
+              <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+            </div>
+            <br/>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
+              <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+            </div>
+              <br/>
+              <button type="button" class="btn-success btn-md buttonstyle">Login</button>
+              <span><button type="button" class="btn-default btn-md buttonstyle">Lupa Password</button></span>
+          </form>
+
+        </div>
+            </div>
+        </div>
+
+      </div>
+
+     </div>
+  </section>
+</div>
+<!--footer start from here-->
+
+<div class="copyright">
+  <div class="container">
+    <div class="col-md-6">
+      <p>© 2017 - BP3 padang all rights reserved</p>
+    </div>
+    <div class="col-md-6">
+      <ul class="bottom_ul">
+        <li><a href="#">bp3padang.com</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Faq's</a></li>
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">Site Map</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+      <script type="text/javascript" src="js/logincontrol.js"></script>
+
+  </body>
+</html>
